1f9620d31dd6eebc4fe39d61dbea0c783f4e4f66
johntelforduk <jtelford1@gmail.com>
2020-07-23 21:41:26 +0100

    Load text from file.

--- README.md
+++ README.md
@@ -4,6 +4,6 @@
 pip install pygame
 ```
 #### TODO
-* Load text from parm file.
-* Make the text repeat once it has scrolled all the way to the left.
 * Press "Q" to close the window, and quit the program.
+* Add Social Image to Github.
+* Make repo public.
--- ticker.py
+++ ticker.py
@@ -1,13 +1,23 @@
 # Scrolling ticker for OBS.
 
+import sys
 import pygame
 
 ticker_colour = [255, 255, 255]         # White.
 background_colour = [0, 255, 0]         # Green (chroma key colour).
-screen_size = [1280, 720]               # Set the height and width of the window.
+screen_size = [1280, 720]               # [width, height].
 ticker_dist_from_bottom = 70            # Number of pixels that ticker is away from bottom of window.
-target_fps = 100                        # Adjust this to speed up / slow down scrolling.
+font_size = 30                          # Size of the scrolling ticker.
+target_fps = 100                        # Adjust this to speed up or slow down scrolling.
+font_factor = 0.6                       # font_factor = width of character in pixels / font_size.
 
+# Read contents of parm filename into text variable.
+filename = sys.argv[1]
+f = open(filename)
+text = f.read()
+
+# TODO Make the text loop once it has scrolled all the way to the left.
+ticker_text = text
 
 pygame.init()                           # Initialize the game engine.
 
@@ -17,7 +27,7 @@ screen = pygame.display.set_mode(screen_size, pygame.NOFRAME)
 clock = pygame.time.Clock()
 
 pygame.font.init()
-ticker_font = pygame.font.SysFont('Courier New', 30)
+ticker_font = pygame.font.SysFont('Courier New', font_size)
 
 text_offset = 0
 
@@ -28,15 +38,17 @@ while not done:
 
     screen.fill(background_colour)
 
-    for event in pygame.event.get():  # User did something.
-        if event.type == pygame.QUIT:  # If user closed window.
-            done = True  # Flag that we are done so we exit this loop, and quit the ticker.
+    for event in pygame.event.get():        # User did something.
+        if event.type == pygame.QUIT:       # If user closed window.
+            done = True                     # Flag that we are done so we exit this loop, and quit the ticker.
 
-    text_surface = ticker_font.render('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum', False, ticker_colour)
+    text_surface = ticker_font.render(ticker_text, False, ticker_colour)
     screen.blit(text_surface, (text_offset, screen_size[1] - ticker_dist_from_bottom))
 
-    text_offset -= 1                    # Scroll the text 1 pixel to the left.
-
     pygame.display.flip()
+
+    text_offset -= 1                        # Scroll the text 1 pixel to the left.
+    if text_offset < - len(ticker_text) * font_size * font_factor:
+        text_offset = 0
 
 pygame.quit()

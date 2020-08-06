19e2d40bd61bf8f9a40c6e2bcf71a059b12d161f
johntelforduk <jtelford1@gmail.com>
2020-08-06 17:05:52 +0100

    Add text_length_in_pixels function.

--- ticker.py
+++ ticker.py
@@ -3,21 +3,35 @@
 import sys
 import pygame
 
+
 ticker_colour = [255, 255, 255]         # White.
 background_colour = [0, 255, 0]         # Green (chroma key colour).
 screen_size = [1280, 720]               # [width, height].
 ticker_dist_from_bottom = 70            # Number of pixels that ticker is away from bottom of window.
 font_size = 30                          # Size of the scrolling ticker.
+font_factor = 0.6  # font_factor = width of character in pixels / font_size.
 target_fps = 100                        # Adjust this to speed up or slow down scrolling.
-font_factor = 0.6                       # font_factor = width of character in pixels / font_size.
+
+
+def text_length_in_pixels(this_text: str, font_point_size: int) -> int:
+    """For parm text string, return its width in number of pixels if it is rendered in parm font size."""
+    return round(len(this_text) * font_point_size * font_factor)
+
 
 # Read contents of parm filename into text variable.
 filename = sys.argv[1]
 f = open(filename)
 text = f.read()
 
-# TODO Make the text loop once it has scrolled all the way to the left.
-ticker_text = text
+# TODO Make short text repeat so it fills screen.
+
+# TODO Add spaces to start of text, so it starts scrolling from right hand side of screen.
+leading_spaces_needed = int(1 + screen_size[0] / (font_size * font_factor))
+print(leading_spaces_needed)
+
+
+
+ticker_text = ' ' * leading_spaces_needed + text
 
 pygame.init()                           # Initialize the game engine.
 
@@ -48,7 +62,9 @@ while not done:
     pygame.display.flip()
 
     text_offset -= 1                        # Scroll the text 1 pixel to the left.
-    if text_offset < - len(ticker_text) * font_size * font_factor:
+
+    # If all of the text has scrolled off left of screen, then reset the ticker.
+    if text_offset < - text_length_in_pixels(ticker_text, font_size):
         text_offset = 0
 
 pygame.quit()
